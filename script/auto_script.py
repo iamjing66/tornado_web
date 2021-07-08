cf47f516cce5918cacf09fb46f1c4320fac4164a
iamjing66 <iamjing66@163.com>
2021-07-08 15:51:03 +0800

    update usernama and upwd into enviroment

--- script/auto_script.py
+++ script/auto_script.py
@@ -1,6 +1,10 @@
 import requests
+import os
 
-data = {"uname": 18235101805, "upwd": "JINGYuhao825", "bushu": ""}
+uname = os.getenv("lv_uname")
+upwd = os.getenv("lv_upwd")
+
+data = {"uname": uname, "upwd": upwd, "bushu": ""}
 
 url = "http://127.0.0.1:8000/bushu"
 res = requests.post(url=url, data=data).text
